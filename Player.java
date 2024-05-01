38232555ad718a6f674021f6dfb89c738b10d999
plezerm <153388968+plezerm@users.noreply.github.com>
2024-05-01 18:14:20 +0200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private final String bet;
+
+    public Player(String bet) {
+        this.bet = bet;
+    }
+
+    public String getBet() {
+        return bet;
+    }
+}
